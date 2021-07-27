d8e0267e878817c117f1538d68a0dbfed77f1e7f
Manoj Kumar <manoj.k0508@gmail.com>
2021-07-27 17:27:27 +0530

    initial folder structure created

---
+++ src/main/java/com/springlearn/reciepewebapp/Spring5RecipeWebappApplication.java
@@ -0,0 +1,13 @@
+package com.springlearn.reciepewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipeWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springlearn/reciepewebapp/Spring5RecipeWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearn.reciepewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
